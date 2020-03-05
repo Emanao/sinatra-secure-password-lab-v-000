496af70abe084633eda2637bf83a71f4ad2467cc
Eva Navarro <emanao@me.com>
2020-03-05 17:51:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,36 +7,6 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "password_security"
   end
-  get "/signup" do
-    erb :signup
-  end
-
-  post "/signup" do
-    user = User.new(:username=> params[:username], :password=> params[:password])
-    if valid_user?(params) && user.save
-      redirect '/login'
-    else
-      redirect '/failure'
-    end
-
-  end
-
-
-
-
-  get "/login" do
-    erb :login
-  end
-
-  post "/login" do
-    user = User.find_by(:username=> params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/account"
-    else
-      redirect "/failure"
-    end
-  end
 
   get "/success" do
     if logged_in?
