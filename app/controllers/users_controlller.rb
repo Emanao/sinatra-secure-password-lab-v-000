019d3d7addb93a455b1d3f574651a3d049d7e4e3
Eva Navarro <emanao@me.com>
2020-03-05 18:07:22 +0000

    Automatically backed up by Learn

--- app/controllers/users_controlller.rb
+++ app/controllers/users_controlller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   get "/" do
     erb :index
   end
+  
   get "/signup" do
     erb :signup
   end
@@ -14,7 +15,7 @@ class UsersController < ApplicationController
       redirect '/failure'
     end
   end
-  
+
   get '/account' do
     @user = User.find(session[:user_id])
     erb :account
