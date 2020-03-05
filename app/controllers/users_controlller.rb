1d61f1ef652919558423d51d6ca477865fc64f52
Eva Navarro <emanao@me.com>
2020-03-05 18:02:33 +0000

    Automatically backed up by Learn

--- app/controllers/users_controlller.rb
+++ app/controllers/users_controlller.rb
@@ -2,6 +2,19 @@ class UsersController < ApplicationController
   get "/" do
     erb :index
   end
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    user = User.new(:username=> params[:username], :password=> params[:password])
+    if valid_user?(params) && user.save
+      redirect '/login'
+    else
+      redirect '/failure'
+    end
+  end
+  
   get '/account' do
     @user = User.find(session[:user_id])
     erb :account
